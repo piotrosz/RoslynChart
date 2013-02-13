a37d02aec2b814f00a05625fa9a07c0ae84fe1e3
Piotr Ludwiczuk <piotrosz@gmail.com>
2013-02-14 00:15:49 +0100

    Added scripting Chart project

---
+++ ChartScript/Controllers/ChartController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.DataVisualization.Charting;
+using ChartScript.Models;
+using System.IO;
+using System.Drawing;
+using Roslyn.Scripting.CSharp;
+
+namespace ChartScript.Controllers
+{
+    public class ChartController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public FileContentResult DrawChart()
+        {
+            var viewModel = new ChartData();
+            Chart chart = viewModel.BarChart;
+            return ReturnChart(chart);
+        }
+
+        [HttpPost]
+        public FileContentResult DrawChart(string code)
+        {
+            var engine = new ScriptEngine();
+            
+            new[]  
+                {    
+                    typeof(System.ComponentModel.Component).Assembly,
+                    typeof (int).Assembly,
+                    typeof (HttpContext).Assembly,
+                    typeof (Color).Assembly,
+                    typeof (Chart).Assembly,  
+                    typeof (IEnumerable<>).Assembly,  
+                    typeof (IQueryable).Assembly
+                }.ToList().ForEach(asm => engine.AddReference(asm));
+
+            new[]  
+                {  
+                   "System", 
+                   "System.Web",
+                   "System.Drawing",
+                   "System.Linq",   
+                   "System.Collections",  
+                   "System.Collections.Generic",
+                   "System.Web.UI.DataVisualization.Charting"
+                }.ToList().ForEach(ns => engine.ImportNamespace(ns));
+
+            var session = engine.CreateSession();
+
+            session.Execute(code);
+            var resultingChart = (Chart) session.Execute("CreateChart()");
+
+            return ReturnChart(resultingChart);
+        }
+
+        private FileContentResult ReturnChart(Chart chart)
+        {
+            MemoryStream ms = new MemoryStream();
+            chart.SaveImage(ms, ChartImageFormat.Png);
+            ms.Position = 0;
+            return File(ms.GetBuffer(), "image/png");
+        }
+
+    }
+}
---
+++ ChartScript/Models/ChartData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.DataVisualization.Charting;
+
+namespace ChartScript.Models
+{
+    public class ChartData
+    {
+        public Chart BarChart
+        {
+            get { return BuildBarChart(); }
+        }
+
+        private Chart BuildBarChart()
+        {
+            return BindChartData();
+        }
+
+        private Chart BindChartData()
+        {
+            Chart chart = new Chart();
+            chart.Width = 150;
+            chart.Height = 150;
+
+            chart.ChartAreas.Add(new ChartArea());
+            chart.Series.Add(new Series());
+
+            chart.Series[0].ChartType = SeriesChartType.Column;
+            chart.Series[0].BackGradientStyle = GradientStyle.HorizontalCenter;
+            chart.Series[0].BackSecondaryColor = System.Drawing.Color.Black;
+
+            var xValues = new int[] { 1, 2, 3, 4 };
+            var yValues = new double[] { 0.3, 0.3, 0.1, 0.7 };
+
+            for (int i = 0; i < xValues.Length; i++)
+            {
+                int x = xValues[i];
+                double y = yValues[i];
+                int ptIdx = chart.Series[0].Points.AddXY(x, y);
+                DataPoint pt = chart.Series[0].Points[ptIdx];
+            }
+
+            return chart;
+
+        }
+    }
+
+}
