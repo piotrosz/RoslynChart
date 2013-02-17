639bf066702b6208f2e3f2094e29bff1f85d7c11
Piotr Ludwiczuk <piotrosz@gmail.com>
2013-02-17 23:59:10 +0100

    Added codesample, codesamplelist classes

---
+++ ChartScript/Models/CodeSampleList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChartScript.Models
+{
+    public class CodeSampleList : List<CodeSample>
+    {
+    }
+}
--- ChartScript/Scripts/chartmodel.js
+++ ChartScript/Scripts/chartmodel.js
@@ -1,4 +1,23 @@
-﻿// TODO: Use knockout
+﻿var CodeSample = (function () {
+    function CodeSample(name, code) {
+        this.Name = name;
+        this.Code = code;
+    }
+    return CodeSample;
+})();
+
+var CodeSampleList = (function () {
+
+    // TODO: get code samples from server
+    function CodeSampleList() {
+        this.Samples = ko.observableArray([
+            new CodeSample("Line chart", "code"),
+            new CodeSample("Bar chart", "code")
+        ]);
+    }
+    return CodeSampleList;
+})();
+
 
 // Encodes C# code from the editor
 function fixedEncodeURIComponent(str) {
