24302f798515220fd4e1a45ec6f7eaee6a32995b
Piotr Ludwiczuk <piotrosz@gmail.com>
2013-03-04 00:18:56 +0100

    Added stacked bar chart code sample

---
+++ ChartScript/SampleCode/Chart types/Bar and column charts/BarColumn.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.DataVisualization.Charting;
+
+namespace ChartScript.SampleCode.Chart_types.Bar_and_column_charts
+{
+    public class BarColumn
+    {
+        public Chart GetChart()
+        {
+            // [Bar, column]
+            Chart chart = new Chart();
+
+
+
+            //<asp:CHART id="Chart1" runat="server" Palette="BrightPastel" BackColor="#F3DFC1" Width="412px" Height="296px" BorderlineDashStyle="Solid" BackGradientStyle="TopBottom" BorderWidth="2" BorderColor="181, 64, 1">
+            //                <titles>
+            //                    <asp:Title ShadowColor="32, 0, 0, 0" Font="Trebuchet MS, 14.25pt, style=Bold" ShadowOffset="3" Text="Column Chart" Name="Title1" ForeColor="26, 59, 105"></asp:Title>
+            //                </titles>
+            //                <legends>
+            //                    <asp:Legend TitleFont="Microsoft Sans Serif, 8pt, style=Bold" BackColor="Transparent" Font="Trebuchet MS, 8.25pt, style=Bold" IsTextAutoFit="False" Enabled="False" Name="Default"></asp:Legend>
+            //                </legends>
+            //                <borderskin SkinStyle="Emboss"></borderskin>
+            //                <series>
+            //                    <asp:Series XValueType="DateTime" Name="Series1" BorderColor="180, 26, 59, 105">
+            //                        <points>
+            //                            <asp:DataPoint XValue="36890" YValues="32" />
+            //                            <asp:DataPoint XValue="36891" YValues="56" />
+            //                            <asp:DataPoint XValue="36892" YValues="35" />
+            //                            <asp:DataPoint XValue="36893" YValues="12" />
+            //                            <asp:DataPoint XValue="36894" YValues="35" />
+            //                            <asp:DataPoint XValue="36895" YValues="6" />
+            //                            <asp:DataPoint XValue="36896" YValues="23" />
+            //                        </points>
+            //                    </asp:Series>
+            //                    <asp:Series XValueType="DateTime" Name="Series2" BorderColor="180, 26, 59, 105">
+            //                        <points>
+            //                            <asp:DataPoint XValue="36890" YValues="67" />
+            //                            <asp:DataPoint XValue="36891" YValues="24" />
+            //                            <asp:DataPoint XValue="36892" YValues="12" />
+            //                            <asp:DataPoint XValue="36893" YValues="8" />
+            //                            <asp:DataPoint XValue="36894" YValues="46" />
+            //                            <asp:DataPoint XValue="36895" YValues="14" />
+            //                            <asp:DataPoint XValue="36896" YValues="76" />
+            //                        </points>
+            //                    </asp:Series>
+            //                </series>
+            //                <chartareas>
+            //                    <asp:ChartArea Name="ChartArea1" BorderColor="64, 64, 64, 64" BackSecondaryColor="White" BackColor="OldLace" ShadowColor="Transparent" BackGradientStyle="TopBottom">
+            //                        <area3dstyle Rotation="10" Perspective="10" Inclination="15" IsRightAngleAxes="False" WallWidth="0" IsClustered="False" />
+            //                        <axisy LineColor="64, 64, 64, 64"  LabelAutoFitMaxFontSize="8">
+            //                            <LabelStyle Font="Trebuchet MS, 8.25pt, style=Bold" Format="C0" />
+            //                            <MajorGrid LineColor="64, 64, 64, 64" />
+            //                        </axisy>
+            //                        <axisx LineColor="64, 64, 64, 64"  LabelAutoFitMaxFontSize="8">
+            //                            <LabelStyle Font="Trebuchet MS, 8.25pt, style=Bold" IsEndLabelVisible="False" Format="MM-dd" />
+            //                            <MajorGrid LineColor="64, 64, 64, 64" />
+            //                        </axisx>
+            //                    </asp:ChartArea>
+            //                </chartareas>
+            //            </asp:CHART>
+
+            // Set series chart type
+            chart.Series["Default"].ChartType = SeriesChartType.Bar;
+
+            // Set series point width
+            chart.Series["Default"]["PointWidth"] = "0.6";
+
+            // Show data points labels
+            chart.Series["Default"].IsValueShownAsLabel = true;
+
+            // Set data points label style
+            chart.Series["Default"]["BarLabelStyle"] = "Center";
+
+            // Show as 3D
+            chart.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = true;
+
+            // Draw as 3D Cylinder
+            chart.Series["Default"]["DrawingStyle"] = "Cylinder";
+
+
+            return chart;
+            // End
+        }
+    }
+}
---
+++ ChartScript/SampleCode/Chart types/Bar and column charts/Stacked.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI.DataVisualization.Charting;
+
+namespace ChartScript.SampleCode.Chart_types.Bar_and_column_charts
+{
+    public class Stacked
+    {
+        public Chart GetChart()
+        {
+// [Stacked]
+Chart chart = new Chart();
+chart.Height = 296;
+chart.Width = 412;
+chart.BackColor = ColorTranslator.FromHtml("#D3DFF0");
+chart.Palette = ChartColorPalette.EarthTones;
+chart.BorderlineDashStyle = ChartDashStyle.DashDotDot;
+chart.BackGradientStyle = GradientStyle.DiagonalRight;
+chart.BorderlineWidth = 2;
+chart.BorderlineColor = Color.FromArgb(26, 59, 105);
+
+chart.Legends.Add(new Legend { 
+    TitleFont = new Font("Microsoft Sans Serif", 8f, FontStyle.Bold),
+    BackColor = Color.Transparent,
+    Font = new Font("Trebuchet MS", 8.25f, FontStyle.Bold),
+    IsTextAutoFit = false,
+    Enabled = false,
+    Name = "Default"
+});
+
+chart.BorderSkin.SkinStyle = BorderSkinStyle.FrameThin4;
+
+chart.Series.Add(new Series
+{
+    Name = "Series1",
+    ChartType = SeriesChartType.StackedArea100,
+    BorderColor = Color.FromArgb(180, 26, 59, 105),
+    Color = Color.FromArgb(220, 65, 140, 240)
+});
+
+chart.Series.Add(new Series
+{
+    Name = "Series2", 
+    ChartType = SeriesChartType.StackedArea100,
+    BorderColor = Color.FromArgb(180, 26, 59, 105),
+    Color = Color.FromArgb(220, 252, 180, 65)
+});
+
+chart.Series.Add(new Series
+{
+    Name = "Series3",
+    ChartType = SeriesChartType.StackedArea100,
+    BorderColor = Color.FromArgb(180, 26, 59, 105),
+    Color = Color.Fuchsia
+});
+
+chart.Series.Add(new Series
+{
+    Name = "Series4",
+    ChartType = SeriesChartType.StackedArea100,
+    BorderColor = Color.FromArgb(180, 26, 59, 105),
+    Color = Color.FromArgb(220, 5, 100, 146)
+});
+
+chart.ChartAreas.Add(new ChartArea
+{
+    Name = "ChartArea1",
+    BorderColor = Color.FromArgb(64, 64, 64, 64),
+    BorderDashStyle = ChartDashStyle.Solid,
+    BackSecondaryColor = Color.Transparent,
+    BackColor = Color.FromArgb(64, 165, 191, 228),
+    ShadowColor = Color.Transparent,
+    BackGradientStyle = GradientStyle.LeftRight,
+});
+
+chart.ChartAreas[0].Area3DStyle.Rotation = 40;
+chart.ChartAreas[0].Area3DStyle.Inclination = 15;
+chart.ChartAreas[0].Area3DStyle.WallWidth = 2;
+
+chart.ChartAreas[0].Position.X = 3;
+chart.ChartAreas[0].Position.Y = 3;
+chart.ChartAreas[0].Position.Height = 92;
+chart.ChartAreas[0].Position.Width = 92;
+
+chart.ChartAreas[0].AxisX.LineColor = Color.Turquoise;
+chart.ChartAreas[0].AxisX.LabelAutoFitMaxFontSize = 8;
+
+chart.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.Red;
+
+chart.ChartAreas[0].AxisY.LineColor = Color.YellowGreen;
+chart.ChartAreas[0].AxisY.LabelAutoFitMaxFontSize = 8;
+
+// Populate series data
+Random random = new Random();
+for (int pointIndex = 0; pointIndex < 10; pointIndex++)
+{
+    chart.Series["Series1"].Points.AddY(random.Next(45, 95));
+    chart.Series["Series2"].Points.AddY(random.Next(0, 40));
+    chart.Series["Series3"].Points.AddY(random.Next(2, 95));
+    chart.Series["Series4"].Points.AddY(random.Next(100, 200));
+}
+
+// Set chart type
+chart.Series["Series1"].ChartType = SeriesChartType.StackedArea100;
+
+// Show point labels
+chart.Series["Series1"].IsValueShownAsLabel = true;
+
+// Disable X axis margin
+chart.ChartAreas["ChartArea1"].AxisX.IsMarginVisible = false;
+
+// Enable 3D
+chart.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = true;
+
+// Set the first two series to be grouped into Group1
+chart.Series[0]["StackedGroupName"] = "Group1";
+chart.Series[1]["StackedGroupName"] = "Group1";
+
+// Set the last two series to be grouped into Group2
+chart.Series[2]["StackedGroupName"] = "Group2";
+chart.Series[3]["StackedGroupName"] = "Group2";
+
+return chart;
+// End
+        }
+    }
+}
